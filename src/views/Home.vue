a53625608374b47d61023205bf33d3a3a6235301
Jirayut Srisawangwong <jiras457@cubts.postbox.in.th>
2020-05-25 18:07:00 +0700

    start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <div id="home" class="align-center">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <h1>COVID19 TRACKER WEB</h1>
+    </div>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
