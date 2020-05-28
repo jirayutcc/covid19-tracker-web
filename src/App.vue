83415801a23fa67b5cfd99f2ee9e3ff0db08f5b0
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2020-05-28 11:12:45 +0700

    delete Home page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
-  <v-app id="app">
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+  <v-container>
+    <v-app id="app">
+      <v-content>
+        <img alt="Vue logo" src="@/assets/covid19.png" />
+        <h1>COVID19 TRACKER WEB</h1>
+      </v-content>
+    </v-app>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
   name: "App",
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      data: [],
+      errors: [],
+    };
+  },
+  created() {
+    axios
+      .get(`https://covid19.mathdro.id/api`)
+      .then((response) => {
+        this.data = response.data;
+        //console.log(":= ", this.data)
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
 };
 </script>
 
 <style scoped>
-  * {
-    font-family: 'Jua', sans-serif;
-  }
+* {
+  font-family: "Jua", sans-serif;
+}
 </style>
